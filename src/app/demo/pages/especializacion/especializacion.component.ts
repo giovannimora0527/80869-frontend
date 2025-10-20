f1ce234351f090be2dcec07ded01fa7dafe5a9cf
884690_DavidVillamizar <davidlinares090@gmail.com>
2025-10-20 13:08:29 -0500

    Update especializacion.component.ts

--- src/app/demo/pages/especializacion/especializacion.component.ts
+++ src/app/demo/pages/especializacion/especializacion.component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
+import { EspecialidadService } from './service/especialidad.service';
+import { Especialidad } from './models/especialidad';
+import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-especializacion',
-  imports: [],
-  templateUrl: './especializacion.component.html',
-  styleUrl: './especializacion.component.scss'
+  selector: 'app-especialidad',
+  imports: [CommonModule],
+  templateUrl: './especialidad.component.html',
+  styleUrl: './especialidad.component.scss'
 })
-export class EspecializacionComponent {
+export class EspecialidadComponent {
+  especialidadList: Especialidad[] = [];
+  constructor(private readonly especialidadService: EspecialidadService) {
+  }
 
+
+  probarBoton(especialidad: Especialidad) {
+    console.log(especialidad);
+  }
 }
