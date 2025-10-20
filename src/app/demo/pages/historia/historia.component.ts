f637a5b320fee32b5362486fb2eb88bbc6173b56
884690_DavidVillamizar <davidlinares090@gmail.com>
2025-10-20 15:07:51 -0500

    Update historia.component.ts

--- src/app/demo/pages/historia/historia.component.ts
+++ src/app/demo/pages/historia/historia.component.ts
@@ -1,11 +1,33 @@
 import { Component } from '@angular/core';
+import { HistorialService } from './service/historial.service';
+import { Historial } from './models/historial';
+import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-historia',
-  imports: [],
-  templateUrl: './historia.component.html',
-  styleUrl: './historia.component.scss'
+  selector: 'app-historial',
+  imports: [CommonModule],
+  templateUrl: './historial.component.html',
+  styleUrl: './historial.component.scss'
 })
-export class HistoriaComponent {
+export class HistorialComponent {
+  historialList: Historial[] = [];
+  constructor(private readonly historialService: HistorialService) {
+    this.listarHistorial();
+  }
 
+  listarHistorial() {
+    this.historialService.listarHistorial().subscribe({
+      next: (data) => {
+        console.log(data);
+        this.historialList = data;
+      },
+      error: (error) => {
+        console.error('Error fetching historial:', error);
+      }
+    });
+  }
+
+  probarBoton(historial: Historial) {
+    console.log(historial);
+  }
 }
