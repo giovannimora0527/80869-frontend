02e4813d0c1909984145a66e78495e33aae46683
884690_DavidVillamizar <davidlinares090@gmail.com>
2025-10-20 12:54:58 -0500

    Update cita.ts

--- src/app/demo/pages/cita/models/cita.ts
+++ src/app/demo/pages/cita/models/cita.ts
@@ -1,1 +1,12 @@
-// Archivo de modelo temporal
+import { Medico } from "../../medico/models/medico";
+import { Paciente } from "../../paciente/models/paciente";
+
+
+export class Cita {
+    id!: number;
+    fechaHora!: string;
+    motivo!: string;
+    estado!: string;
+    paciente!: Paciente;
+    medico!: Medico;
+}
