1e2f94dd0ccd07ac336779b5257bd47dab836a96
884690_DavidVillamizar <davidlinares090@gmail.com>
2025-10-20 13:39:48 -0500

    Update formula.component.ts

--- src/app/demo/pages/formula/formula.component.ts
+++ src/app/demo/pages/formula/formula.component.ts
@@ -1,11 +1,33 @@
 import { Component } from '@angular/core';
+import { RecetaService } from './service/receta.service';
+import { Receta } from './models/receta';
+import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-formula',
-  imports: [],
-  templateUrl: './formula.component.html',
-  styleUrl: './formula.component.scss'
+  selector: 'app-receta',
+  imports: [CommonModule],
+  templateUrl: './receta.component.html',
+  styleUrls: ['./receta.component.scss']
 })
-export class FormulaComponent {
+export class RecetaComponent {
+  recetaList: Receta[] = [];
+  constructor(private readonly recetaService: RecetaService) {
+    this.listarRecetas();
+  }
 
+  listarRecetas() {
+    this.recetaService.listarRecetas().subscribe({
+      next: (data) => {
+        console.log(data);
+        this.recetaList = data;
+      },
+      error: (error) => {
+        console.error('Error fetching recetas:', error);
+      }
+    });
+  }
+
+  probarBoton(receta: Receta) {
+    console.log(receta);
+  }
 }
