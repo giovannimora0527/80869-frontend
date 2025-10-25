c7cdff6f580b8089b5c5113b892e627f8792028a
884690_DavidVillamizar <davidlinares090@gmail.com>
2025-10-25 11:34:14 -0500

    Update medicamento.component.ts

--- src/app/demo/pages/medicamento/medicamento.component.ts
+++ src/app/demo/pages/medicamento/medicamento.component.ts
@@ -1,33 +1,11 @@
 import { Component } from '@angular/core';
-import { MedicamentoService } from './service/medicamento.service';
-import { Medicamento } from './models/medicamento';
-import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-medicamento',
-  imports: [CommonModule],
+  imports: [],
   templateUrl: './medicamento.component.html',
-  styleUrls: ['./medicamento.component.scss']
+  styleUrl: './medicamento.component.scss'
 })
 export class MedicamentoComponent {
-  medicamentoList: Medicamento[] = [];
-  constructor(private readonly medicamentoService: MedicamentoService) {
-    this.listarMedicamentos();
-  }
 
-  listarMedicamentos() {
-    this.medicamentoService.listarMedicamentos().subscribe({
-      next: (data) => {
-        console.log(data);
-        this.medicamentoList = data;
-      },
-      error: (error) => {
-        console.error('Error fetching medicamentos:', error);
-      }
-    });
-  }
-
-  probarBoton(medicamento: Medicamento) {
-    console.log(medicamento);
-  }
 }
