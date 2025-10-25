1b088bc3a821bfbde6ef2686bca592979f056260
884690_DavidVillamizar <davidlinares090@gmail.com>
2025-10-25 11:35:46 -0500

    Update medicamento.ts

--- src/app/demo/pages/medicamento/models /medicamento.ts
+++ src/app/demo/pages/medicamento/models /medicamento.ts
@@ -1,1 +1,10 @@
-
+export class Medicamento{
+    id!: number;
+    nombre!: string;
+    descripcion!: string;
+    presentacion!: string;
+    fechaCompra!: Date;
+    fechaModificacionRegistro!: Date;
+    fechaVence!: Date;
+    fechaCreacionRegistro!: Date;
+}
