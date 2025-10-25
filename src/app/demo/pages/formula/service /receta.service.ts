2ecbbd151a7b2460711a746cb5a329d1c89fe6b8
884690_DavidVillamizar <davidlinares090@gmail.com>
2025-10-25 11:29:34 -0500

    Update receta.service.ts

--- src/app/demo/pages/formula/service /receta.service.ts
+++ src/app/demo/pages/formula/service /receta.service.ts
@@ -1,19 +1,28 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 import { BackendService } from 'src/app/services/backend.service';
 import { environment } from 'src/environments/environment';
-import { Receta } from '../models/receta';
-import { Observable } from 'rxjs/internal/Observable';
+import { Receta } from '../models/formula';
+import { RespuestaRs } from '../../usuario/models/respuesta-rs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RecetaService {
-  apiUrl = environment.apiUrl;
-  endpoint = 'receta';
+  urlBase = environment.apiUrl;
+  endpoint: string = 'receta';
 
   constructor(private readonly backendService: BackendService) {}
 
   listarRecetas(): Observable<Receta[]> {
-    return this.backendService.get(this.apiUrl, this.endpoint, 'listar');
+    return this.backendService.get(this.urlBase, this.endpoint, 'listar');
+  }
+
+  guardarReceta(receta: Receta): Observable<RespuestaRs> {
+    return this.backendService.post(this.urlBase, this.endpoint, 'guardar', receta);
+  }
+
+  actualizarReceta(receta: Receta): Observable<RespuestaRs> {
+    return this.backendService.post(this.urlBase, this.endpoint, 'actualizar', receta);
   }
 }
