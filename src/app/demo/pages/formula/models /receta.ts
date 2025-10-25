9606f88b0a1700845cdfd12ba7f378ca52c865fa
884690_DavidVillamizar <davidlinares090@gmail.com>
2025-10-25 11:28:38 -0500

    Update receta.ts

--- src/app/demo/pages/formula/models /receta.ts
+++ src/app/demo/pages/formula/models /receta.ts
@@ -1,6 +1,11 @@
+import { Cita } from 'src/app/demo/pages/cita/models/cita';
+import { Medicamento } from 'src/app/demo/pages/medicamento/models/medicamento';
+
 export class Receta {
     id!: number;
+    cita!: Cita;
+    medicamento!: Medicamento;
     dosis!: string;
     indicaciones!: string;
-    fechaCreacionRegistro!: string;
+    fechaCreacionRegistro!: Date;
 }
