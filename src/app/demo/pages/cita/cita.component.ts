0f798642a90b63ad06d1afbd1a11625b64f7bf6e
884690_DavidVillamizar <davidlinares090@gmail.com>
2025-10-25 11:22:56 -0500

    Update cita.component.ts

--- src/app/demo/pages/cita/cita.component.ts
+++ src/app/demo/pages/cita/cita.component.ts
@@ -1,33 +1,11 @@
 import { Component } from '@angular/core';
-import { CitaService } from './service/cita.service';
-import { Cita } from './models/cita';
-import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-cita',
-  imports: [CommonModule],
+  imports: [],
   templateUrl: './cita.component.html',
-  styleUrls: ['./cita.component.scss']
+  styleUrl: './cita.component.scss'
 })
 export class CitaComponent {
-  citaList: Cita[] = [];
-  constructor(private readonly citaService: CitaService) {
-    this.listarCitas();
-  }
 
-  listarCitas() {
-    this.citaService.listarCitas().subscribe({
-      next: (data) => {
-        console.log(data);
-        this.citaList = data;
-      },
-      error: (error) => {
-        console.error('Error fetching citas:', error);
-      }
-    });
-  }
-
-  probarBoton(cita: Cita) {
-    console.log(cita);
-  }
 }
