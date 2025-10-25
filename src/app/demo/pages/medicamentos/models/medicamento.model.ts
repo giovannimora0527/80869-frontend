6a7085f7fcd2ebe27fb0d9c943b655564c6fc8ec
Juan Manuel Sarmiento Luis - Soporte SA <jsarmientol@summa-sci.com>
2025-10-25 10:29:23 -0500

    Ultimos cambios

--- src/app/demo/pages/medicamentos/models/medicamento.model.ts
+++ src/app/demo/pages/medicamentos/models/medicamento.model.ts
@@ -3,11 +3,4 @@ export class Medicamento {
   nombre!: string;
   descripcion?: string;
   presentacion?: string;
-  concentracion?: string;
-  stock?: number;
-  precio?: number;
-  fecha_compra?: Date;
-  fecha_vencimiento?: Date;
-  fecha_creacion_registro?: Date;
-  fecha_modificacion_registro?: Date;
 }
