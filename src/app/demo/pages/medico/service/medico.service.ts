0f2ef92138ba63c0dbb8bcc56cce5dfddccfe2d1
884690_DavidVillamizar <davidlinares090@gmail.com>
2025-10-25 11:38:06 -0500

    Update medico.service.ts

--- src/app/demo/pages/medico/service/medico.service.ts
+++ src/app/demo/pages/medico/service/medico.service.ts
@@ -3,26 +3,26 @@ import { Observable } from 'rxjs';
 import { BackendService } from 'src/app/services/backend.service';
 import { environment } from 'src/environments/environment';
 import { Medico } from '../models/medico';
-import { RespuestRs } from '../../usuario/models/respuestars';
+import { RespuestaRs } from '../../usuario/models/respuesta-rs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MedicoService {
-  apiUrl = environment.apiUrl;
-  endpoint = 'medico';
+  urlBase = environment.apiUrl;
+  endpoint: string = 'medico';
 
   constructor(private readonly backendService: BackendService) {}
 
   listarMedicos(): Observable<Medico[]> {
-    return this.backendService.get(this.apiUrl, this.endpoint, 'listar');
+    return this.backendService.get(this.urlBase, this.endpoint, 'listar');
   }
 
-  guardarMedico(medico: Medico): Observable<RespuestRs> {
-    return this.backendService.post(this.apiUrl, this.endpoint, 'guardar', medico);
+  guardarMedico(medico: Medico): Observable<RespuestaRs> {
+    return this.backendService.post(this.urlBase, this.endpoint, 'guardar', medico);
   }
 
-  actualizarMedico(medico: Medico): Observable<RespuestRs> {
-    return this.backendService.post(this.apiUrl, this.endpoint, 'actualizar', medico);
+  actualizarMedico(medico: Medico): Observable<RespuestaRs> {
+    return this.backendService.post(this.urlBase, this.endpoint, 'actualizar', medico);
   }
 }
