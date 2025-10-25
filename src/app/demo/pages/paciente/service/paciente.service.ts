3c7fb1246cd0e5ffecf3dee319e9fc72aa25d04c
884690_DavidVillamizar <davidlinares090@gmail.com>
2025-10-25 11:38:48 -0500

    Update paciente.service.ts

--- src/app/demo/pages/paciente/service/paciente.service.ts
+++ src/app/demo/pages/paciente/service/paciente.service.ts
@@ -1,19 +1,30 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 import { BackendService } from 'src/app/services/backend.service';
 import { environment } from 'src/environments/environment';
 import { Paciente } from '../models/paciente';
-import { Observable } from 'rxjs/internal/Observable';
+import { RespuestaRs } from '../models/respuesta-rs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PacienteService {
-  apiUrl = environment.apiUrl;
-  endpoint = 'paciente';
+  urlBase = environment.apiUrl;
+  endpoint: string = 'paciente';
 
   constructor(private readonly backendService: BackendService) {}
-  
-  listarPacientes(): Observable<Paciente[]> {
-    return this.backendService.get(this.apiUrl, this.endpoint, 'listar');
+
+  listarPaciente(): Observable<Paciente[]> {
+    return this.backendService.get(this.urlBase, this.endpoint, 'listar');
   }
+
+  guardarPaciente(paciente: Paciente): Observable<RespuestaRs> {
+    return this.backendService.post(this.urlBase, this.endpoint, 'guardar', paciente);
+  }
+
+  actualizarPaciente(paciente: Paciente): Observable<RespuestaRs> {
+    return this.backendService.post(this.urlBase, this.endpoint, 'actualizar', paciente);
+  } 
+
+
 }
