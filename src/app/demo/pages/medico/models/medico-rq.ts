9934ee01fcd6ed67588d3ccf8a2d30467847ebb9
884690_DavidVillamizar <davidlinares090@gmail.com>
2025-10-25 11:37:32 -0500

    Delete src/app/demo/pages/medico/models/medico-rq.ts

--- src/app/demo/pages/medico/models/medico-rq.ts
+++
@@ -1,9 +0,0 @@
-export class MedicoRq {
-    tipoDocumento!: string; // El tipo de documento es obligatorio
-    numeroDocumento!: string; // El número de documento es obligatorio
-    nombres!: string; // Los nombres son obligatorios
-    apellidos!: string; // Los apellidos son obligatorios
-    telefono!: string; // El teléfono es obligatorio
-    registroProfesional!: string; // El registro profesional es obligatorio
-    especializacion!: number; // La especialización es obligatoria
-}
