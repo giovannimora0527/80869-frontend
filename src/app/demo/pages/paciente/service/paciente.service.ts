cfea3ff177e0e8d491c6c7f53a44a39d4fbd1ecb
884690_DavidVillamizar <davidlinares090@gmail.com>
2025-10-25 23:40:05 -0500

    Update paciente.service.ts

--- src/app/demo/pages/paciente/service/paciente.service.ts
+++ src/app/demo/pages/paciente/service/paciente.service.ts
@@ -3,7 +3,6 @@ import { Observable } from 'rxjs';
 import { BackendService } from 'src/app/services/backend.service';
 import { environment } from 'src/environments/environment';
 import { Paciente } from '../models/paciente';
-import { RespuestaRs } from '../models/respuesta-rs';
 
 @Injectable({
   providedIn: 'root'
@@ -14,17 +13,23 @@ export class PacienteService {
 
   constructor(private readonly backendService: BackendService) {}
 
-  listarPaciente(): Observable<Paciente[]> {
+  listarPacientes(): Observable<Paciente[]> {
     return this.backendService.get(this.urlBase, this.endpoint, 'listar');
   }
 
-  guardarPaciente(paciente: Paciente): Observable<RespuestaRs> {
-    return this.backendService.post(this.urlBase, this.endpoint, 'guardar', paciente);
+  buscarPorDocumento(documento: string): Observable<Paciente> {
+    return this.backendService.get(this.urlBase, this.endpoint, documento);
   }
 
-  actualizarPaciente(paciente: Paciente): Observable<RespuestaRs> {
-    return this.backendService.post(this.urlBase, this.endpoint, 'actualizar', paciente);
-  } 
+  guardarPaciente(paciente: Paciente): Observable<Paciente> {
+    return this.backendService.post(this.urlBase, this.endpoint, '', paciente);
+  }
 
+  actualizarPaciente(paciente: Paciente): Observable<Paciente> {
+    return this.backendService.put(this.urlBase, this.endpoint, paciente.id.toString(), paciente);
+  }
 
+  listarPacientesOrdenadosPorFechaNacimiento(): Observable<Paciente[]> {
+    return this.backendService.get(this.urlBase, this.endpoint, 'ordenados-por-nacimiento');
+  }
 }
