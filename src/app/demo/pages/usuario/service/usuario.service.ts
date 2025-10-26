7d9e7d1b47b0ebc8c9c2ca2cf559242b07ad51c3
884690_DavidVillamizar <davidlinares090@gmail.com>
2025-10-25 23:43:08 -0500

    Update usuario.service.ts

--- src/app/demo/pages/usuario/service/usuario.service.ts
+++ src/app/demo/pages/usuario/service/usuario.service.ts
@@ -4,7 +4,7 @@ import { BackendService } from 'src/app/services/backend.service';
 import { environment } from 'src/environments/environment';
 import { Usuario } from '../models/usuario';
 import { RespuestaRs } from '../models/respuesta-rs';
-
+import { HttpParams } from '@angular/common/http';
 @Injectable({
   providedIn: 'root'
 })
@@ -23,8 +23,27 @@ export class UsuarioService {
   }
 
   actualizarUsuario(usuario: Usuario): Observable<RespuestaRs> {
+    // POST a /usuario/actualizar
     return this.backendService.post(this.urlBase, this.endpoint, 'actualizar', usuario);
-  } 
+  }
 
+  eliminarUsuario(id: number): Observable<RespuestaRs> {
+    // DELETE a /usuario/eliminar?id=ID
+    return this.backendService.delete(this.urlBase, this.endpoint, 'eliminar', { id });
+  }
 
+  buscarUsuarioPorNombre(nombre: string): Observable<Usuario> {
+    const params = new HttpParams().set('nombre', nombre);
+    return this.backendService.get<Usuario>(this.urlBase, this.endpoint, 'buscar-nombre', params);
+  }
+
+  listarUsuariosPorRol(rol: string): Observable<Usuario[]> {
+    const params = new HttpParams().set('rol', rol);
+    return this.backendService.get<Usuario[]>(this.urlBase, this.endpoint, 'listar-rol', params);
+  }
+
+  buscarUsuariosPorEstado(activo: number): Observable<Usuario[]> {
+    const params = new HttpParams().set('activo', activo.toString());
+    return this.backendService.get<Usuario[]>(this.urlBase, this.endpoint, 'buscar-estado', params);
+  }
 }
